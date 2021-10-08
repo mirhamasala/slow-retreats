5530db8b3f02c73f018960a4bf8a6f9ead63aab3
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2021-10-08 15:16:58 +0100

    Move logic to document (#4)
    * Apply background-color to body
    
    * Move site title and favicon to document

--- pages/_document.js
+++ pages/_document.js
@@ -10,12 +10,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Slow Retreats</title>
+          <link rel="icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap"
             rel="stylesheet"
           />
         </Head>
-        <body className="font-raleway">
+        <body className="font-raleway bg-gray-900">
           <Main />
           <NextScript />
         </body>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,6 @@
-import Head from 'next/head';
-
 export default function Home() {
   return (
-    <div className="bg-gray-900">
-      <Head>
-        <title>Slow Retreats</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <>
       <main className="flex flex-col items-start justify-center w-full min-h-screen px-20 bg-hero bg-cover">
         <h1 className="pt-24 text-6xl font-bold text-gray-200">
           Slow Retreats
@@ -17,6 +10,6 @@ export default function Home() {
       <footer className="flex items-center justify-center w-full h-12 text-gray-100">
         <span>&copy; {new Date().getFullYear()}</span>
       </footer>
-    </div>
+    </>
   );
 }
