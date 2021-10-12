249e25156b8eb457914fdb566a80050d48a08af1
Mirha Masala <mirha.masala@me.com>
2021-10-12 17:44:00 +0100

    Adapt container spacings for different devices

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Rules from '../components/Rules';
 export default function Home() {
   return (
     <>
-      <div className="grid min-h-screen width-screen-80 py-12 px-screen-10 bg-hero bg-cover">
+      <div className="grid min-h-screen py-12 px-screen-10 bg-hero bg-cover lg:pl-screen-20">
         <div className="content-start grid gap-y-3 text-right sm:content-center sm:gap-y-5">
           <h2 className="text-lg font-medium tracking-widest text-gray-900 uppercase whitespace-nowrap sm:text-xl">
             Reset Edition
@@ -22,15 +22,15 @@ export default function Home() {
       </div>
 
       <main className="text-normal text-gray-300 leading-relaxed tracking-wide sm:text-lg">
-        <div className="min-h-screen px-screen-10 py-12">
+        <div className="min-h-screen px-screen-10 lg:px-screen-20 py-12">
           <Program />
         </div>
-        <div className="min-h-screen px-screen-10 py-12 bg-gray-800">
+        <div className="min-h-screen px-screen-10 lg:px-screen-20 py-12 bg-gray-800">
           <div>
             <PackingList />
           </div>
         </div>
-        <div className="min-h-screen px-screen-10 py-12">
+        <div className="min-h-screen px-screen-10 lg:px-screen-20 py-12">
           <Rules />
         </div>
       </main>
