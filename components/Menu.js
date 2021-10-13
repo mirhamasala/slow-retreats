6006f29194dbd16ec8de8798ba66e00a0ac3bfb4
Mirha Masala <mirha.masala@me.com>
2021-10-13 09:29:01 +0100

    Use simple a tag

--- components/Menu.js
+++ components/Menu.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { sections } from '../constants/sections';
 
 function Menu() {
@@ -6,11 +5,12 @@ function Menu() {
     <ul className="space-y-2 text-lg sm:space-y-3 sm:text-xl">
       {sections.map((section) => (
         <li key={section.id}>
-          <Link href={`#${section.id}`}>
-            <a className="text-gray-700 capitalize whitespace-nowrap hover:text-gray-800 focus:text-gray-900">
-              {section.title} <span>&#8212;</span>
-            </a>
-          </Link>
+          <a
+            href={`#${section.id}`}
+            className="text-gray-700 capitalize whitespace-nowrap hover:text-gray-800 focus:text-gray-900"
+          >
+            {section.title} <span>&#8212;</span>
+          </a>
         </li>
       ))}
     </ul>
