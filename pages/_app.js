519197b73a29c27ff9024aa9061dc4b8f8448887
Mirha Masala <mirha.masala@me.com>
2021-10-13 09:28:56 +0100

    Include smoothscroll-polyfill package

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import 'tailwindcss/tailwind.css';
+import smoothscrollPolyfill from 'smoothscroll-polyfill';
 import 'smoothscroll-anchor-polyfill';
+
+if (typeof window !== 'undefined') {
+  smoothscrollPolyfill.polyfill();
+}
 
 /* eslint-disable react/prop-types */
 function MyApp({ Component, pageProps }) {
