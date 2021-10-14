132a28fbe21b1b3e7616258ccdcdf3e9b21d2150
Mirha Masala <mirha.masala@me.com>
2021-10-14 19:14:13 +0100

    Fix background color Packing List

--- components/Section.js
+++ components/Section.js
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 
 function Section({ id, title, children }) {
-  const bgShade = id === 'packing-list' ? '800' : '900';
+  const bgColor = id === 'packing-list' ? 'bg-gray-800' : 'bg-gray-900';
 
   return (
     <section
       id={id}
-      className={`relative min-h-screen pt-7 pb-10 text-normal text-gray-300 leading-relaxed tracking-wide bg-gray-${bgShade} sm:pt-6 sm:pb-12 sm:text-lg`}
+      className={`relative min-h-screen pt-7 pb-10 text-normal text-gray-300 leading-relaxed tracking-wide ${bgColor} sm:pt-6 sm:pb-12 sm:text-lg`}
     >
       <h2
-        className={`sticky top-0 py-3 px-screen-10 text-2xl text-gray-100 capitalize tracking-wider bg-gray-${bgShade} bg-opacity-80 sm:py-6 lg:px-screen-20  sm:text-4xl`}
+        className={`sticky top-0 py-3 px-screen-10 text-2xl text-gray-100 capitalize tracking-wider ${bgColor} bg-opacity-80 sm:py-6 lg:px-screen-20  sm:text-4xl`}
       >
         {title}
       </h2>
