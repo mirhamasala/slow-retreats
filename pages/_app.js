09880163fbc852814f582d691df64cce59ead42c
Mirha Masala <mirha.masala@me.com>
2021-11-07 18:48:58 +0000

    Move site title

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
+import Head from "next/head";
 import 'tailwindcss/tailwind.css';
 import smoothscrollPolyfill from 'smoothscroll-polyfill';
 import 'smoothscroll-anchor-polyfill';
+
 
 if (typeof window !== 'undefined') {
   smoothscrollPolyfill.polyfill();
@@ -8,7 +10,14 @@ if (typeof window !== 'undefined') {
 
 /* eslint-disable react/prop-types */
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Slow Retreats</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,6 @@ class MyDocument extends Document {
     return (
       <Html className="smooth-scroll">
         <Head>
-          <title>Slow Retreats</title>
           <link rel="icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap"
