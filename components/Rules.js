16e15777622155aa3ee5145d16704c5c085eb8dd
Mirha Masala <mirha.masala@me.com>
2021-11-07 17:14:16 +0000

    Change wording Rules

--- components/Rules.js
+++ components/Rules.js
@@ -8,7 +8,7 @@ function Rules() {
       <div className="mb-6 space-y-3 max-w-prose">
         <p>
           During our four days together, we'll strengthen the roots within
-          ourselves and our circle with rituals and exercises that will nourish
+          ourselves and our circle with rituals and exercises that will benefit
           us long after the retreat.
         </p>
         <p>
@@ -20,7 +20,7 @@ function Rules() {
           The gift of presence is super powerful. By being present for others,
           we also allow our own lives to be immersed in the <i>Now</i>. It's a
           state of being typically quite hidden from us, buried in our busyness,
-          that we'll give space to reveal itself slowly.
+          that we'll now give space to reveal itself slowly.
         </p>
         <p>
           To be able to step into this flow, we need to nourish our little
@@ -30,21 +30,23 @@ function Rules() {
       </div>
       <ul className="list-decimal space-y-3 max-w-prose">
         <li>
-          We'll switch off and hand over our electronic devices (phones, iPads,
-          laptops, etc.) on day one of the retreat.
+          We'll switch off and hand over our electronic devices (phones,
+          tablets, laptops, etc.) on day one of the retreat.
           <p className="mt-2 italic">
-            This means we won't have any contact with our friends and family
-            during the retreat, nor will we have access to any work-related
-            messages, social media, etc. If you need to check in with home, a
-            phone will be available for a 5-minute phone call.
+            This means we won't have any access to social media, work-related
+            messages, or contact with our friends and family during the retreat. If you need to check in with home, a phone will be available for a
+            5-minute phone call.
           </p>
         </li>
         <li>
-          We won't take photos. To remember the retreat, we'll take four
-          Polaroids each day at a place of our choosing.
+          We won't take any photos, except for four Polaroids each day at a
+          place of our choosing.
         </li>
-        <li>We'll have lunch and dinner together during which there'll be one conversation.</li>
-        <li>We'll try not to complain.</li>
+        <li>
+          We'll have lunch and dinner together, during which there'll be one
+          conversation.
+        </li>
+        <li>We'll do our best not to complain.</li>
       </ul>
     </Section>
   );
